5ca564297664e23c9af8164aab68990748b64795
Lorenzo <lorenzofg24@gmail.com>
2021-06-21 16:39:40 +0200

    importing pokemon card component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { Pokemon } from '../../../models/pokemon';
 import humps from 'humps';
+import PokemonCard from '../pokemon/card/card';
 
 function App() {
   const [pokemon, setPokemon] = useState<Pokemon>(null);
@@ -19,33 +20,17 @@ function App() {
     init();
   }, []);
 
-  const getArtworkUrl = (pokemon: Pokemon) => {
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
-  };
+  // const getArtworkUrl = (pokemon: Pokemon) => {
+  //   return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+  // };
 
-  const zeroPad = (id: number, places: number) => {
-    return String(id).padStart(places, '0');
-  };
+  // const zeroPad = (id: number, places: number) => {
+  //   return String(id).padStart(places, '0');
+  // };
 
   return (
     <section>
-      <div className="card">
-        <h2 className="card__title">{pokemon?.name}</h2>
-
-        <div className="card__number">{`#${zeroPad(pokemon?.id, 3)}`}</div>
-
-        <figure className="card__figure">
-          <img
-            className="card__figure__image"
-            src={pokemon && getArtworkUrl(pokemon)}
-            alt={pokemon?.name}
-          />
-        </figure>
-
-        <div className="card__type-container">
-          <div className="card__type">{pokemon?.types[0].type.name}</div>
-        </div>
-      </div>
+      <PokemonCard pokemon={pokemon} />
     </section>
   );
 }
