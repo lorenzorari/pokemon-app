819ecc0d490acac9c9cdeb1361fe8dca1437eac1
Lorenzo <lorenzofg24@gmail.com>
2021-06-22 12:11:35 +0200

    Importing scss module

--- src/components/pokemon/card/card.tsx
+++ src/components/pokemon/card/card.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { getArtworkUrl } from '../../../helpers/get-artwork-url';
 import { zeroPad } from '../../../libs/utils/zero-pad';
-import './card.scss';
+import styles from './card.module.scss';
+import PokemonCardTitle from './title';
 
 const PokemonCard = ({ pokemon }) => {
   return (
-    <div className="card">
-      <h2 className="card__title">{pokemon?.name}</h2>
+    <div className={styles.card}>
+      <PokemonCardTitle pokemon={pokemon} />
 
-      <div className="card__number">{`#${zeroPad(pokemon?.id, 3)}`}</div>
+      <div className={styles.card__number}>{`#${zeroPad(pokemon?.id, 3)}`}</div>
 
-      <figure className="card__figure">
+      <figure className={styles.card__figure}>
         <img
-          className="card__figure__image"
+          className={styles.card__figure__image}
           src={pokemon && getArtworkUrl(pokemon)}
           alt={pokemon?.name}
         />
       </figure>
 
-      <div className="card__type-container">
-        <div className="card__type">{pokemon?.types[0].type.name}</div>
+      <div className={styles.card__typeContainer}>
+        <div className={styles.card__type}>{pokemon?.types[0].type.name}</div>
       </div>
     </div>
   );
