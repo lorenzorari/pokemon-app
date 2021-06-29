6dec4dc5ca61f35eade98cc7f34e9e51904da9ec
Lorenzo <lorenzofg24@gmail.com>
2021-06-29 21:10:15 +0200

    Setting the card title theme

--- src/components/pokemon/card/title/index.tsx
+++ src/components/pokemon/card/title/index.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { setCSSProperty } from '../../../../libs/utils/set-css-property';
 import styles from './title.module.scss';
 
 const PokemonCardTitle = ({ pokemon }) => {
-  return <h2 className={styles.title}>{pokemon?.name}</h2>;
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  const setBackgroundColor = (type: string) => {
+    setCSSProperty(
+      titleRef.current,
+      'background-color',
+      `var(--color-${type}-1)`
+    );
+  };
+
+  const setTheme = (type: string) => {
+    setBackgroundColor(type);
+  };
+
+  useEffect(() => setTheme(pokemon?.types[0].type.name));
+
+  return (
+    <h2 ref={titleRef} className={styles.title}>
+      {pokemon?.name}
+    </h2>
+  );
 };
 
 export default PokemonCardTitle;
