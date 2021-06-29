47ebbd5a53aa0cf3137d265cce130d00aa26a26e
Lorenzo <lorenzofg24@gmail.com>
2021-06-29 23:26:05 +0200

    Setting the card theme

--- src/components/pokemon/card/card.tsx
+++ src/components/pokemon/card/card.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PokemonCardId from './id';
 import PokemonCardImage from './image';
 import PokemonCardTitle from './title';
 import TypeTag from '../../type-tag';
 import styles from './card.module.scss';
+import { setCSSProperty } from '../../../libs/utils/set-css-property';
 
 const PokemonCard = ({ pokemon }) => {
+  const cardRef = useRef<HTMLHeadingElement>(null);
+
+  const setBackgroundColor = (type: string) => {
+    setCSSProperty(
+      cardRef.current,
+      'background-color',
+      `var(--color-${type}-2)`
+    );
+  };
+
+  const setTheme = (type: string) => {
+    setBackgroundColor(type);
+  };
+
+  useEffect(() => setTheme(pokemon?.types[0].type.name));
+
   return (
-    <div className={styles.card}>
+    <div ref={cardRef} className={styles.card}>
       <PokemonCardTitle pokemon={pokemon} />
 
       <PokemonCardId pokemon={pokemon} />
