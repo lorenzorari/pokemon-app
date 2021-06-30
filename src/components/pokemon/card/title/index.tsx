9a917b5d49a7bc313888c1808433691a14dfd544
Lorenzo <lorenzofg24@gmail.com>
2021-06-30 19:55:35 +0200

    Deleting color settings

--- src/components/pokemon/card/title/index.tsx
+++ src/components/pokemon/card/title/index.tsx
@@ -1,29 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import { setCSSProperty } from '../../../../libs/utils/set-css-property';
+import React from 'react';
 import styles from './title.module.scss';
 
 const PokemonCardTitle = ({ pokemon }) => {
-  const titleRef = useRef<HTMLHeadingElement>(null);
-
-  // const setBackgroundColor = (type: string) => {
-  //   setCSSProperty(
-  //     titleRef.current,
-  //     'background-color',
-  //     `var(--color-${type}-1)`
-  //   );
-  // };
-
-  // const setTheme = (type: string) => {
-  //   setBackgroundColor(type);
-  // };
-
-  // useEffect(() => setTheme(pokemon?.types[0].type.name));
-
-  return (
-    <h2 ref={titleRef} className={styles.title}>
-      {pokemon?.name}
-    </h2>
-  );
+  return <h2 className={styles.title}>{pokemon?.name}</h2>;
 };
 
 export default PokemonCardTitle;
