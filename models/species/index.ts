a671af5d80fbf93b7c62a667592c336530a2cbec
Lorenzo <lorenzofg24@gmail.com>
2021-07-26 21:22:47 +0200

    Species interface created

---
+++ models/species/index.ts
@@ -0,0 +1,35 @@
+import { NamedAPIResource } from './../named-api-resource';
+import { SpeciesDexEntries } from './dex-entry';
+import { Genera } from './genus';
+import { PalParkEncounterAreas } from './pal-park-encounter-area';
+import { SpeciesVarieties } from './variety';
+
+interface Species {
+  id?: number;
+  name?: string;
+  order?: number;
+  genderRate?: number;
+  captureRate?: number;
+  baseHappiness?: number;
+  isBaby?: boolean;
+  isLegendary?: boolean;
+  isMythical?: boolean;
+  hatchCounter?: number;
+  hasGenderDifferences?: boolean;
+  formsSwitchable?: boolean;
+  growthRate?: NamedAPIResource;
+  pokedexNumbers?: SpeciesDexEntries;
+  eggGroups?: NamedAPIResource;
+  color?: NamedAPIResource;
+  shape?: NamedAPIResource;
+  evolvesFromSpecies?: NamedAPIResource;
+  evolutionChain?: any;
+  habitat?: NamedAPIResource;
+  generation?: NamedAPIResource;
+  names?: any;
+  palParkEncounters?: PalParkEncounterAreas;
+  flavorTextEntries?: any;
+  formDescriptions?: any;
+  genera?: Genera;
+  varieties?: SpeciesVarieties;
+}
