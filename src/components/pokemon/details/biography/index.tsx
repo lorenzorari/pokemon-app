1e180755f183f3659d9159c6e4532ee565e8ceb3
Lorenzo <lorenzofg24@gmail.com>
2021-07-28 18:43:13 +0200

    Removing details-container and tabs

--- src/components/pokemon/details/biography/index.tsx
+++ src/components/pokemon/details/biography/index.tsx
@@ -5,28 +5,13 @@ import styles from './biography.module.scss';
 
 const PokemonDetailsBiography = () => {
   return (
-    <div className={styles['details-container']}>
-      <ul className={styles['details-tabs']}>
-        <li>Biography</li>
-        <li>Stats</li>
-        <li>Evolutions</li>
-      </ul>
-
-      <PokemonDescription>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
-        doloremque dolore vero, ipsa quidem, cupiditate corrupti numquam,
-        doloribus quisquam magni animi eius dicta nostrum repudiandae esse
-        placeat aspernatur? Maxime, quia.
-      </PokemonDescription>
-
-      <ul className={styles['info-container']}>
-        <PokemonInformation title="Species">content</PokemonInformation>
-        <PokemonInformation title="Height">content</PokemonInformation>
-        <PokemonInformation title="Weight">content</PokemonInformation>
-        <PokemonInformation title="Abilities">content</PokemonInformation>
-        <PokemonInformation title="Gender">content</PokemonInformation>
-      </ul>
-    </div>
+    <ul className={styles['info-container']}>
+      <PokemonInformation title="Species">content</PokemonInformation>
+      <PokemonInformation title="Height">content</PokemonInformation>
+      <PokemonInformation title="Weight">content</PokemonInformation>
+      <PokemonInformation title="Abilities">content</PokemonInformation>
+      <PokemonInformation title="Gender">content</PokemonInformation>
+    </ul>
   );
 };
 
