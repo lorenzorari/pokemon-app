d7f35bf82d105617b7b3b1db545118db04d4926e
Lorenzo <lorenzofg24@gmail.com>
2021-08-02 18:46:06 +0200

    Displaying dynamic data

--- src/components/pokemon/details/stats/stats.tsx
+++ src/components/pokemon/details/stats/stats.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Pokemon } from '../../../../../models/pokemon';
 import Stat from '../../../stat';
 import styles from './stats.module.scss';
 
-const PokemonDetailsStats = () => {
+interface Props {
+  pokemon: Pokemon;
+}
+
+const replaceDashBySpace = (word: string) => {
+  return word.replace('-', ' ');
+};
+
+const PokemonDetailsStats = ({ pokemon }: Props) => {
+  const stats = pokemon.stats.map(({ stat, baseStat }) => {
+    const statName = replaceDashBySpace(stat.name);
+    return { title: statName, value: baseStat };
+  });
+
   return (
     <section className={styles['stats-container']}>
-      <Stat title="HP" value="70" />
-      <Stat title="Attack" value="70" />
-      <Stat title="Defense" value="70" />
-      <Stat title="Special Attack" value="70" />
-      <Stat title="Special Defense" value="70" />
-      <Stat title="Speed" value="70" />
+      {stats.map((stat, i) => (
+        <Stat
+          key={i}
+          title={stat.title === 'hp' ? 'HP' : stat.title}
+          value={stat.value}
+        />
+      ))}
     </section>
   );
 };
