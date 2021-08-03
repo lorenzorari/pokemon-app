adc09b25fc80684ed54ed03e5a6837336e19d096
Lorenzo <lorenzofg24@gmail.com>
2021-08-03 23:41:10 +0200

    Mapping through tabs and displaying them

--- src/pages/pokemon-details/index.tsx
+++ src/pages/pokemon-details/index.tsx
@@ -1,27 +1,22 @@
+import humps from 'humps';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { ReactSVG } from 'react-svg';
 import { Pokemon } from '../../../models/pokemon';
-import PokemonCardId from '../../components/pokemon/card/id';
-import PokemonCardImage from '../../components/pokemon/card/image';
-import PokemonCardTitle from '../../components/pokemon/card/title';
-import PokemonDetailsBiography from '../../components/pokemon/details/biography';
-import Tab from '../../components/tab';
-import TypeTag from '../../components/type-tag';
-import { getPokemon } from '../../services/pokemon';
 import { Species } from '../../../models/species';
-import styles from './pokemon-details.module.scss';
-import humps from 'humps';
-import PokemonDetailsStats from '../../components/pokemon/details/stats/stats';
 import PokemonCard from '../../components/pokemon/card/card';
+import PokemonDetailsBiography from '../../components/pokemon/details/biography';
+import PokemonDetailsStats from '../../components/pokemon/details/stats/stats';
+import Tab from '../../components/tab';
+import { getPokemon } from '../../services/pokemon';
+import styles from './pokemon-details.module.scss';
 
 interface Params {
   id: string;
 }
 
-const BIOGRAPHY = 'biography';
-const STATS = 'stats';
-const EVOLUTIONS = 'evolutions';
+const BIOGRAPHY = 'Biography';
+const STATS = 'Stats';
+const EVOLUTIONS = 'Evolutions';
 
 const PokemonDetails = () => {
   const { id } = useParams<Params>();
@@ -29,17 +24,14 @@ const PokemonDetails = () => {
   const [species, setSpecies] = useState<Species>(null);
   const [activeTab, setActiveTab] = useState<string>(STATS);
 
+  const tabs: string[] = [BIOGRAPHY, STATS, EVOLUTIONS];
+
   // const pokemonType = pokemon?.types[0].type.name;
 
   // const style = {
   //   '--color-type-1': `var(--color-${pokemonType}-1)`,
   //   '--color-type-2': `var(--color-${pokemonType}-2)`,
   // } as React.CSSProperties;
-
-  const styleTypeTag = {
-    padding: '.5rem',
-    margin: '.5rem .5rem 0 0',
-  };
 
   const tabView = {
     [BIOGRAPHY]: species && (
@@ -78,24 +70,15 @@ const PokemonDetails = () => {
 
           <div className={styles['details-container']}>
             <ul className={styles['details-tabs']}>
-              <Tab
-                onClick={() => setActiveTab(BIOGRAPHY)}
-                isActive={activeTab === BIOGRAPHY}
-              >
-                Biography
-              </Tab>
-              <Tab
-                onClick={() => setActiveTab(STATS)}
-                isActive={activeTab === STATS}
-              >
-                Stats
-              </Tab>
-              <Tab
-                onClick={() => setActiveTab(EVOLUTIONS)}
-                isActive={activeTab === EVOLUTIONS}
-              >
-                Evolutions
-              </Tab>
+              {tabs.map((tab, i) => (
+                <Tab
+                  key={i}
+                  onClick={() => setActiveTab(tab)}
+                  isActive={activeTab === tab}
+                >
+                  {tab}
+                </Tab>
+              ))}
             </ul>
 
             {tabView[activeTab]}
