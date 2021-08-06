1033b702e35df4fe7d9ea928ba07f07a14bddee3
Lorenzo <lorenzofg24@gmail.com>
2021-08-06 23:47:32 +0200

    Getting pokemon evolutions

--- src/pages/pokemon-details/index.tsx
+++ src/pages/pokemon-details/index.tsx
@@ -1,12 +1,14 @@
 import humps from 'humps';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { Pokemon } from '../../../models/pokemon';
+import { updateExpressionWithTypeArguments } from 'typescript';
+import { Pokemon, Pokemons } from '../../../models/pokemon';
 import { Species } from '../../../models/species';
 import PokemonCard from '../../components/pokemon/card/card';
 import PokemonDetailsBiography from '../../components/pokemon/details/biography';
 import PokemonDetailsStats from '../../components/pokemon/details/stats';
 import Tab from '../../components/tab';
+import { getArtworkUrl } from '../../helpers/get-artwork-url';
 import { getPokemon } from '../../services/pokemon';
 import styles from './pokemon-details.module.scss';
 
@@ -18,10 +20,16 @@ const BIOGRAPHY = 'Biography';
 const STATS = 'Stats';
 const EVOLUTIONS = 'Evolutions';
 
+const getIdFromSpeciesResourceUrl = (url: string) => {
+  return +url.slice(-2)[0];
+};
+
 const PokemonDetails = () => {
   const { id } = useParams<Params>();
   const [pokemon, setPokemon] = useState<Pokemon>(null);
   const [species, setSpecies] = useState<Species>(null);
+  // const [evolutionChain, setEvolutionChain] = useState(null);
+  const [pokemonEvolutions, setPokemonEvolutions] = useState([]);
   const [activeTab, setActiveTab] = useState<string>(STATS);
 
   const tabs: string[] = [BIOGRAPHY, STATS, EVOLUTIONS];
@@ -60,6 +68,63 @@ const PokemonDetails = () => {
     init();
   }, []);
 
+  const getPokemonEvolutions = evolutionChain => {
+    if (evolutionChain) {
+      const { url, name } = evolutionChain.species; // First species
+
+      const evolutions = [
+        {
+          id: getIdFromSpeciesResourceUrl(url),
+          name,
+        },
+      ];
+
+      let currentEvo = evolutionChain;
+
+      if (currentEvo.evolvesTo.length === 1) {
+        while (currentEvo.evolvesTo.length > 0) {
+          const nextEvo = currentEvo.evolvesTo[0];
+          const { url, name } = nextEvo.species;
+
+          evolutions.push({ id: getIdFromSpeciesResourceUrl(url), name });
+
+          currentEvo = nextEvo;
+        }
+      }
+
+      if (currentEvo.evolvesTo.length > 1) {
+        const alternateEvos = currentEvo.evolvesTo.map(evo => ({
+          id: getIdFromSpeciesResourceUrl(evo.species.url),
+          name: evo.species.name,
+        }));
+
+        evolutions.push(alternateEvos);
+      }
+
+      return evolutions;
+    }
+
+    return null;
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      const { chain }: any = await new Promise(resolve => {
+        fetch(species.evolutionChain.url)
+          .then(res => res.json())
+          .then(res => {
+            const data = humps.camelizeKeys(res);
+            return resolve(data);
+          });
+      });
+
+      const evolutions = getPokemonEvolutions(chain);
+      setPokemonEvolutions(evolutions);
+    };
+
+    species && init();
+  }, [species]);
+
   return (
     <main className={styles.main}>
       {pokemon && (
@@ -82,7 +147,16 @@ const PokemonDetails = () => {
                 ))}
               </ul>
 
-              {tabView[activeTab]}
+              {/* {tabView[activeTab]} */}
+              {pokemonEvolutions.map(evo => {
+                if (Array.isArray(evo)) {
+                  return evo.map(alternateEvo => (
+                    <div key={alternateEvo.id}>{alternateEvo.name}</div>
+                  ));
+                }
+
+                return <div key={evo.id}>{evo.name}</div>;
+              })}
             </div>
           </div>
         </>
