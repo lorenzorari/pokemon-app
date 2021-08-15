9329599bf0cc7127b3544e96ba46068cea175906
Lorenzo <lorenzofg24@gmail.com>
2021-08-15 22:56:42 +0200

    Displaying pokemon evolutions + design

--- src/components/pokemon/details/evolutions/index.tsx
+++ src/components/pokemon/details/evolutions/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import PokemonEvolution from '../../evolution';
+import styles from './evolutions.module.scss';
 
-const PokemonDetailsEvolutions = () => {
-  return <div>evo</div>;
+const PokemonDetailsEvolutions = ({ pokemonEvolutions }) => {
+  return (
+    <ul className={styles['evolutions-container']}>
+      {pokemonEvolutions.map(evo => {
+        if (Array.isArray(evo)) {
+          return (
+            <li>
+              <div className={styles.line}>
+                <hr />
+              </div>
+              <div className={styles['alternate-evolutions-container']}>
+                {evo.map(alternateEvo => (
+                  <PokemonEvolution
+                    key={alternateEvo.id}
+                    evolution={alternateEvo}
+                  />
+                ))}
+              </div>
+            </li>
+          );
+        }
+
+        return (
+          <li key={evo.id}>
+            <div className={styles.line}>
+              <hr />
+            </div>
+            <div className={styles['alternate-evolutions-container']}>
+              <PokemonEvolution evolution={evo} />
+              {/* <PokemonEvolution evolution={evo} /> */}
+              {/* <PokemonEvoluwtion evolution={evo} /> */}
+              {/* <PokemonEvolution evolution={evo} /> */}
+            </div>
+
+            {/* {i !== pokemonEvolutions.length - 1 && <span>{'>'}</span>} */}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default PokemonDetailsEvolutions;
