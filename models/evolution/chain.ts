db281ab3c33ee6239f4d541f68888738ea88f2ed
Lorenzo <lorenzofg24@gmail.com>
2021-08-16 17:51:23 +0200

    EvolutionChain interface created

---
+++ models/evolution/chain.ts
@@ -0,0 +1,19 @@
+import { NamedAPIResource } from './../named-api-resource';
+import { EvolutionDetails } from './detail';
+
+export interface EvolutionChain {
+  id: number;
+  babyTriggerItem: NamedAPIResource;
+  chain: ChainLink;
+}
+
+export declare type EvolutionChains = EvolutionChain[];
+
+export interface ChainLink {
+  isBaby: boolean;
+  species: NamedAPIResource;
+  evolutionDetails: EvolutionDetails;
+  evolvesTo: ChainLinks;
+}
+
+export declare type ChainLinks = ChainLink[];
