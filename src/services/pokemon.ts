790bdaac7c3868d9ad3118b8ef3f46f5fb43c648
Lorenzo <lorenzofg24@gmail.com>
2021-08-18 01:11:28 +0200

    Replacing fetch by axios in getPokemon()

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import humps from 'humps';
 import { Pokemon } from '../../models/pokemon';
 import { PokemonPagination } from './../../models/pokemon/pagination';
@@ -5,14 +6,12 @@ import { PokemonPagination } from './../../models/pokemon/pagination';
 const INITIAL_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 const getPokemon = async (pokemon: string): Promise<Pokemon> => {
-  return new Promise(resolve => {
-    fetch(`${INITIAL_URL}/${pokemon}`)
-      .then(res => res.json())
-      .then(res => {
-        const data = humps.camelizeKeys(res); // Convert underscore to camelCase
-        resolve(data as Pokemon);
-      });
-  });
+  return new Promise(resolve =>
+    axios.get(`${INITIAL_URL}/${pokemon}`).then(res => {
+      const data = humps.camelizeKeys(res.data); // Convert underscore to camelCase
+      resolve(data as Pokemon);
+    })
+  );
 };
 
 const getAllPokemons = async (url: string): Promise<PokemonPagination> => {
