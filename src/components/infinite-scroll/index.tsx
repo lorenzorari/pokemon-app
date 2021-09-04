3ffac8e6c57cb5c09678119a46962b622a22e8dd
Lorenzo <lorenzofg24@gmail.com>
2021-09-05 00:03:06 +0200

    InfiniteScroll component created

---
+++ src/components/infinite-scroll/index.tsx
@@ -0,0 +1,39 @@
+import React, { forwardRef, FunctionComponent, useEffect } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+  observerCallback: IntersectionObserverCallback;
+  page: number;
+  loaderElement: JSX.Element;
+  loadMore: () => Promise<void>;
+}
+
+const InfiniteScroll = forwardRef(
+  (
+    { children, observerCallback, page, loadMore, loaderElement }: Props,
+    ref: React.MutableRefObject<any>
+  ) => {
+    const Loader = () => loaderElement;
+
+    useEffect(() => {
+      const observer = new IntersectionObserver(observerCallback);
+
+      if (ref.current) observer.observe(ref.current);
+    });
+
+    useEffect(() => {
+      if (page > 1) loadMore();
+    }, [page]);
+
+    return (
+      <>
+        {children}
+        <Loader />
+      </>
+    );
+  }
+);
+
+InfiniteScroll.displayName = 'InfiniteScroll';
+
+export default InfiniteScroll;
