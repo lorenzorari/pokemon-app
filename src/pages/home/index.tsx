48e68ec33a4849b8d66c4f07784b8fb2272c8c1a
Lorenzo <lorenzofg24@gmail.com>
2021-09-09 22:21:48 +0200

    Changing fragment to section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -70,7 +70,7 @@ const Home = () => {
       <HomepageHeadingContainer />
 
       {!loading && pokemons.length ? (
-        <>
+        <section>
           <InfiniteScroll
             observerCallback={handleObserver}
             loadMore={loadMore}
@@ -93,7 +93,7 @@ const Home = () => {
               ))}
             </div>
           </InfiniteScroll>
-        </>
+        </section>
       ) : (
         <div>Loading...</div>
       )}
