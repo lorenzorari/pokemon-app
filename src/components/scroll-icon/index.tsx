fc5eac0e5263ea8582da256a6694a99c2d16e765
Lorenzo <lorenzofg24@gmail.com>
2021-09-09 22:56:15 +0200

    Adding cursor style

--- src/components/scroll-icon/index.tsx
+++ src/components/scroll-icon/index.tsx
@@ -6,12 +6,19 @@ import styles from './scroll-icon.module.scss';
 interface Props {
   src: string;
   className?: string;
+  onClick?: () => void;
 }
 
-const ScrollIcon = ({ src, className }: Props) => {
+const ScrollIcon = ({ src, className, onClick }: Props) => {
+  const style = {
+    cursor: 'pointer',
+  } as React.CSSProperties;
+
   return (
     <ReactSVG
+      style={onClick ? style : {}}
       className={classNames(styles['scroll-icon'], className)}
+      onClick={onClick}
       src={src}
     />
   );
