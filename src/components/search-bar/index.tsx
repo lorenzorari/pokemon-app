276114ed34726cb97aee9305f184cfee38edab45
Lorenzo <lorenzofg24@gmail.com>
2021-09-14 18:03:32 +0200

    Adding className prop

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,7 +1,9 @@
 import React, { ChangeEventHandler, KeyboardEventHandler } from 'react';
+import classNames from 'classnames';
 import styles from './search-bar.module.scss';
 
 interface Props {
+  className?: string;
   type?: string;
   placeholder?: string;
   onChange?: ChangeEventHandler<any>;
@@ -10,7 +12,7 @@ interface Props {
 
 const SearchBar = (props: Props) => {
   return (
-    <div className={styles['search-bar']}>
+    <div className={classNames(styles['search-bar'], props.className)}>
       <input
         type={props.type}
         placeholder={props.placeholder}
