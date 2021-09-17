162e988e087cf03f44f3b5fd5fb6e37e5a122abc
Lorenzo <lorenzofg24@gmail.com>
2021-09-17 12:25:30 +0200

    Adding className prop

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { ReactSVG } from 'react-svg';
+import classNames from 'classnames';
 import styles from './button.module.scss';
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
   theme?: 'default' | 'back';
   onClick?: () => void;
 }
 
-const Button = ({ children, theme = 'default', onClick }: Props) => {
+const Button = ({ children, className, theme = 'default', onClick }: Props) => {
   return (
     <>
       {theme === 'default' ? (
-        <button onClick={onClick}>{children}</button>
+        <button className={className} onClick={onClick}>
+          {children}
+        </button>
       ) : (
-        <button className={styles['btn-back']} onClick={onClick}>
+        <button
+          className={classNames(styles['btn-back'], className)}
+          onClick={onClick}
+        >
           <ReactSVG src="/assets/svg/arrow.svg" />
           <span>{children}</span>
         </button>
