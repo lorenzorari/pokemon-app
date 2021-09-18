a9172357f4c12d3a01ff741f53cb439ab1969199
Lorenzo <lorenzofg24@gmail.com>
2021-09-18 15:34:10 +0200

    Creating Autocomplete component + css

---
+++ src/components/autocomplete/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import SearchBar from 'src/components/search-bar';
+import styles from './autocomplete.module.scss';
+
+const Autocomplete = ({ dataToFilter }) => {
+  const history = useHistory();
+  const [searchValue, setSearchValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleChangeSearch = (e: React.FormEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+
+    if (value.length > 0) {
+      const filteredData = dataToFilter.filter(item => item.includes(value));
+
+      setSuggestions(filteredData);
+    }
+
+    setSearchValue(value);
+  };
+
+  const search = async (value: string) => {
+    if (value === '') return;
+
+    history.push(`/pokemon/${value}`);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      search(searchValue);
+    }
+  };
+
+  return (
+    <form className={styles.form}>
+      <SearchBar
+        type="text"
+        className={styles['search-bar']}
+        placeholder="Search a pokemon by name or id..."
+        onChange={handleChangeSearch}
+        onKeyPress={handleKeyPress}
+      />
+
+      <ul className={styles['suggestion-list']}>
+        {suggestions.map((suggestion, i) => (
+          <li key={i}>{suggestion}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default Autocomplete;
