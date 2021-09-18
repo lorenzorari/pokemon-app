d8a07a1c7dd71894f1cdb9075ceacb58bc836ffa
Lorenzo <lorenzofg24@gmail.com>
2021-09-18 22:53:06 +0200

    Creating Suggestions + css

---
+++ src/containers/autocomplete/suggestions/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { getIdFromResourceUrl } from 'src/helpers/get-id-from-resource-url';
+import {
+  NamedAPIResource,
+  NamedAPIResources,
+} from 'src/models/named-api-resource';
+import styles from './suggestions.module.scss';
+
+interface Props {
+  suggestions: NamedAPIResources;
+  suggestionSelected: number;
+  onClickSuggestion?: (suggestion: NamedAPIResource) => void;
+}
+
+const Suggestions = ({
+  suggestions,
+  suggestionSelected,
+  onClickSuggestion,
+}: Props) => {
+  return (
+    <ul className={styles['suggestions']}>
+      {suggestions.map((suggestion, i) => (
+        <li
+          key={i}
+          className={suggestionSelected === i ? styles.selected : ''}
+          onClick={() => onClickSuggestion(suggestion)}
+        >
+          #{getIdFromResourceUrl(suggestion.url)} {suggestion.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Suggestions;
