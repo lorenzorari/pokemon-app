f551154c3cbbbdc8325b08eaee8221ad860fe2d6
Lorenzo Ferrari <lorenzofg24@gmail.com>
2024-06-29 00:46:53 +0200

    Display right anthropometry

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,18 +1,24 @@
-import axios from 'axios';
-import { Pokemon } from 'src/models/pokemon';
-import { PokemonPagination } from 'src/models/pokemon/pagination';
-import { pokeapi } from 'src/helpers/http';
+import axios from "axios";
+import { Pokemon } from "src/models/pokemon";
+import { PokemonPagination } from "src/models/pokemon/pagination";
+import { pokeapi } from "src/helpers/http";
+import { getAnthropometry } from "src/helpers/getAnthropometry";
 
-const INITIAL_URL = 'https://pokeapi.co/api/v2/pokemon';
+const INITIAL_URL = "https://pokeapi.co/api/v2/pokemon";
 
 const getPokemon = async (id: string): Promise<Pokemon> => {
-  return await pokeapi.get(`pokemon/${id}`).json<Pokemon>();
+  const pokemonData = await pokeapi.get(`pokemon/${id}`).json<Pokemon>();
+
+  pokemonData.height = getAnthropometry(pokemonData.height ?? -1);
+  pokemonData.weight = getAnthropometry(pokemonData.weight ?? -1);
+
+  return pokemonData;
 };
 
 const getAllPokemons = async (
   offset?: number,
   limit?: number,
-  url: string = INITIAL_URL
+  url: string = INITIAL_URL,
 ): Promise<PokemonPagination> => {
   if (offset || limit) {
     url = INITIAL_URL + `?offset=${offset}&limit=${limit}`;
