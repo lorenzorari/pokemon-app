27e3b501edadc730e62769c710dd23ab6cb1f7a3
Lorenzo Ferrari <lorenzofg24@gmail.com>
2024-10-14 22:26:31 +0200

    Fix main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "./Navbar";
+import { Navbar } from './Navbar';
 
 interface Props {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ export function MainLayout({ children }: Props) {
   return (
     <>
       <Navbar />
-      <main className="mx-auto 2xl:max-w-[1440px]">{children}</main>
+      <main className="mx-auto pt-8 2xl:max-w-[1440px]">{children}</main>
     </>
   );
 }
