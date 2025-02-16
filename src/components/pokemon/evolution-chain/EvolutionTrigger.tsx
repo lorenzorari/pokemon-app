f086eb8f5d770fa18c22fe7b10cd26f84704a021
Lorenzo Ferrari <lorenzofg24@gmail.com>
2025-02-16 15:25:45 +0100

    Hide trigger if null

--- src/components/pokemon/evolution-chain/EvolutionTrigger.tsx
+++ src/components/pokemon/evolution-chain/EvolutionTrigger.tsx
@@ -5,5 +5,7 @@ export function EvolutionTrigger() {
 
   const trigger = details?.[0].minLevel || null;
 
+  if (!trigger) return null;
+
   return <div className="z-10 rounded-md bg-gray-100 px-2 py-1">Level {trigger}</div>;
 }
